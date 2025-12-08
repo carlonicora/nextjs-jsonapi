6586691719fdffd7cc689d707afe59a53e499935
Carlo Nicora <carlo.nicora@gmail.com>
2025-12-08 23:30:52 +0100

    refactor: move ContributorsList to nextjs-jsonapi

---
+++ src/features/user/components/lists/ContributorsList.tsx
@@ -0,0 +1,41 @@
+"use client";
+
+import { Modules } from "../../../../core";
+import { usePageUrlGenerator } from "../../../../hooks";
+import { Link } from "../../../../shadcnui";
+import { ContentInterface } from "../../../content";
+import { UserInterface } from "../../data";
+import { UserAvatar } from "../widgets";
+
+type ContributorsListProps = {
+  content: ContentInterface;
+};
+
+export function ContributorsList({ content }: ContributorsListProps) {
+  const generateUrl = usePageUrlGenerator();
+
+  return (
+    <div className="flex flex-row items-center">
+      <Link
+        key={content.author.id}
+        href={generateUrl({ page: Modules.User, id: content.author.id })}
+        onClick={(e) => e.stopPropagation()}
+      >
+        <UserAvatar user={content.author} className="mr-1 h-6 w-6" />
+      </Link>
+      <div className="flex flex-row-reverse justify-end -space-x-1 space-x-reverse">
+        {content.editors
+          .filter((editor: UserInterface) => editor.id !== content.author.id)
+          .map((editor: UserInterface) => (
+            <Link
+              key={editor.id}
+              href={generateUrl({ page: Modules.User, id: editor.id })}
+              onClick={(e) => e.stopPropagation()}
+            >
+              <UserAvatar user={editor} className="h-5 w-5" />
+            </Link>
+          ))}
+      </div>
+    </div>
+  );
+}
