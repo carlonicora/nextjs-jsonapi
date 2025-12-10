00438c34c12d485ec366ded2eb56aa8a80b56f98
Carlo Nicora <carlo.nicora@gmail.com>
2025-12-10 12:01:03 +0100

    refactor: move CommonContext to nextjs-jsonapi

---
+++ src/contexts/CommonContext.tsx
@@ -0,0 +1,52 @@
+"use client";
+
+import { useTranslations } from "next-intl";
+
+import { createContext, ReactNode, useContext } from "react";
+import { UserInterface } from "../features";
+import { useCurrentUserContext } from "../features/user/contexts";
+import { BreadcrumbItemData } from "../interfaces";
+import { SharedProvider } from "./SharedContext";
+
+interface CommonContextType {}
+
+const CommonContext = createContext<CommonContextType | undefined>(undefined);
+
+type CommonProviderProps = {
+  children: ReactNode;
+};
+
+export const CommonProvider = ({ children }: CommonProviderProps) => {
+  const { company } = useCurrentUserContext<UserInterface>();
+  const t = useTranslations();
+
+  const breadcrumb = () => {
+    const response: BreadcrumbItemData[] = [];
+
+    return response;
+  };
+
+  const title = () => {
+    const response: any = {
+      type: t(`generic.title`),
+    };
+
+    if (company) response.element = company.name;
+
+    return response;
+  };
+
+  return (
+    <SharedProvider value={{ breadcrumbs: breadcrumb(), title: title() }}>
+      <CommonContext.Provider value={{}}>{children}</CommonContext.Provider>
+    </SharedProvider>
+  );
+};
+
+export const useCommonContext = (): CommonContextType => {
+  const context = useContext(CommonContext);
+  if (context === undefined) {
+    throw new Error("useCommonContext must be used within a CommonProvider");
+  }
+  return context;
+};
--- src/contexts/index.ts
+++ src/contexts/index.ts
@@ -1,2 +1,3 @@
-export * from "./SharedContext";
 export * from "../features/user/contexts/CurrentUserContext";
+export * from "./CommonContext";
+export * from "./SharedContext";
