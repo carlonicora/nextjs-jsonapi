3b2d99f037afb58561ac360b44e74c0a3606477b
Carlo Nicora <carlo.nicora@gmail.com>
2025-12-27 12:30:04 +0100

    feat: add InputOTP component and update exports in shadcnui

--- src/shadcnui/index.ts
+++ src/shadcnui/index.ts
@@ -22,6 +22,7 @@ export * from "./ui/dropdown-menu";
 export * from "./ui/form";
 export * from "./ui/hover-card";
 export * from "./ui/input";
+export * from "./ui/input-otp";
 export * from "./ui/label";
 export * from "./ui/navigation-menu";
 export * from "./ui/popover";
