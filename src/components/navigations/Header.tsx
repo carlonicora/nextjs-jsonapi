2986a39df7c96f9daee14599d088897ec29b85dc
Carlo Nicora <carlo.nicora@gmail.com>
2026-02-23 22:23:05 +0100

    fix: correct linting

--- src/components/navigations/Header.tsx
+++ src/components/navigations/Header.tsx
@@ -6,13 +6,16 @@ import { BreadcrumbNavigation } from "./Breadcrumb";
 
 type HeaderProps = {
   children?: React.ReactNode;
+  className?: string;
 };
 
-export function Header({ children }: HeaderProps) {
+export function Header({ children, className }: HeaderProps) {
   const { breadcrumbs } = useSharedContext();
 
   return (
-    <header className={`sticky top-0 z-10 flex h-12 flex-col items-center justify-start gap-x-4 border-b`}>
+    <header
+      className={`sticky top-0 z-10 flex h-12 flex-col items-center justify-start gap-x-4 border-b ${className ?? ""}`}
+    >
       <div className="bg-sidebar flex h-12 w-full flex-row items-center justify-between pl-2 pr-4">
         <SidebarTrigger aria-label="Toggle sidebar" id="sidebar-trigger" />
         <div className="flex w-full flex-row items-center justify-start">
