79a80a3364d1b859aef4eb11fb5cfd1802e42e24
Carlo Nicora <carlo.nicora@gmail.com>
2026-03-13 00:02:12 +0100

    feat(TableCellAvatar): add className prop for customizable styling

--- src/components/TableCellAvatar.tsx
+++ src/components/TableCellAvatar.tsx
@@ -1,14 +1,16 @@
-import { getInitials } from "../utils/getInitials";
+import { cn } from "@/utils";
 import { Avatar, AvatarFallback, AvatarImage } from "../shadcnui";
+import { getInitials } from "../utils/getInitials";
 
 type TableCellAvatarProps = {
   image?: string;
   name: string;
+  className?: string;
 };
 
-export function TableCellAvatar({ image, name }: TableCellAvatarProps) {
+export function TableCellAvatar({ image, name, className }: TableCellAvatarProps) {
   return (
-    <Avatar className="mr-2 h-10 w-10 shrink-0">
+    <Avatar className={cn("mr-2 h-10 w-10 shrink-0", className)}>
       {image ? <AvatarImage src={image} alt={name} /> : null}
       <AvatarFallback className="text-xs">{getInitials(name)}</AvatarFallback>
     </Avatar>
