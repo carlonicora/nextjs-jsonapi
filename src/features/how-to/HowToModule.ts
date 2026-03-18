92b188be4e49d641a765aff43c18dc6471ae3b46
Carlo Nicora <carlo.nicora@gmail.com>
2026-03-18 16:02:00 +0100

    feat(how-to): add moduleId and icon to HowToModule

--- src/features/how-to/HowToModule.ts
+++ src/features/how-to/HowToModule.ts
@@ -1,12 +1,15 @@
+import { LifeBuoyIcon } from "lucide-react";
 import { createJsonApiInclusion } from "../../core";
 import { ModuleFactory } from "../../permissions";
 import { HowTo } from "./data/HowTo";
 
 export const HowToModule = (factory: ModuleFactory) =>
   factory({
+    moduleId: "6f975207-0df3-4c0d-b541-ed5dc04487b2",
     pageUrl: "/administration/howtos",
     name: "howtos",
     model: HowTo,
+    icon: LifeBuoyIcon,
     inclusions: {
       lists: {
         fields: [createJsonApiInclusion("howtos", [`name`, `description`, `pages`])],
